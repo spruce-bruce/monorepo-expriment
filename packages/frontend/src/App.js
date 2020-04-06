0d84536659a3e75a0d71dc4b1ed5cbb6a816cd7c
spruce-bruce <aaron@synapsestudios.com>
2020-04-06 11:07:00 -0700

    Yarn v1 - working

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import shared from 'shared';
 
-
 function App() {
   return (
     <div className="App">
@@ -18,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React { shared.KEY }
         </a>
       </header>
     </div>
